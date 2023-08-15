8bd316eca939a1412396b9faa4f49f47b5684941
Reholly <eu.ustiantsev@yandex.ru>
2023-08-15 20:15:59 +0500

    some ref

--- Server/Services/ArticleServices/ArticleService.cs
+++ Server/Services/ArticleServices/ArticleService.cs
@@ -6,20 +6,21 @@ namespace Server.Services.ArticleServices;
 public class ArticleService : IArticleService
 {
     private readonly IRepository<Article> _articlesRepository;
-    private readonly int pageArticlesCount = 15; // заменить на конфиг потом
-
-    public ArticleService(IRepository<Article> articlesRepository)
+    private readonly IConfiguration _config;
+    public ArticleService(IRepository<Article> articlesRepository, IConfiguration config)
     {
         _articlesRepository = articlesRepository;
+        _config = config;
     }
     
     public async Task<ICollection<Article>> GetArticles(int page)
     {
         var articles = await _articlesRepository.GetTableAsync();
-        return articles;/*
+        var pageArticlesCount = int.Parse(_config.GetValue<string>("articleOnPages") ?? throw new InvalidOperationException());
+        return articles
             .Skip(page * pageArticlesCount)
             .Take(articles.Count - page * pageArticlesCount)
-            .ToArray();*/
+            .ToArray();
     }
 
     public Task<Article> GetArticle(int id)
